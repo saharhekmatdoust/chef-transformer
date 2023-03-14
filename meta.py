ddc02a69c144de15419bbba73eb65b338eed0479
saharhekmatdoust <s.hekmatdoust@gmail.com>
2023-03-14 16:32:25 -0400

    test

--- meta.py
+++ meta.py
@@ -19,15 +19,7 @@ PROMPT_BOX = "Add custom ingredients here (separated by `,`): "
 STORY = """
 <div class="story-box font-body">
 <p>
-Hello everyone 👋, I am <strong>Chef Transformer</strong>, 
-the owner of this restaurant. I was made by a group of <a href="https://huggingface.co/flax-community/t5-recipe-generation#team-members">NLP Engineers</a> to train my two prodigy recipe creators: <strong>Chef Scheherazade</strong> and <strong>Chef Giovanni</strong>. 
-Both of my students participated in my rigorous culinary program, <a href="https://huggingface.co/flax-community/t5-recipe-generation">T5 fine-tuning</a>, 
-to learn how to prepare exquisite cuisines from a wide variety of ingredients. 
-I've never been more proud of my students -- both can produce exceptional dishes but I regard Scheherazade as being <em>creative</em> while Giovanni is <em>meticulous</em>. 
-If you give each of them the same ingredients, they'll usually come up with something different. <br /><br />
-At the start of the program the chefs read cookbooks containing thousands of recipes of varying difficulties and from cultures all over the world. 
-The NLP engineers helped guide the learning process so that the chefs could actually learn which ingredients work well together rather than just memorize recipes. 
-I trained my chefs by asking them to generate a title, a list of ingredients (including amounts!), and a list of directions after giving them just a simple list of food items. 
+Hello everyone this is sahar and boshra working on this app 👋, the main source for this app is streamlit galley and https://github.com/chef-transformer/chef-transformer
 </p>
 
 <pre>[Inputs]
